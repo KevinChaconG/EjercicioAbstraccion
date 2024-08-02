10fd11fbf6aeeb375d209b599bebb3e46dec2f59
Kevin Chacon <kevin27chacon@gmail.com>
2024-08-02 10:26:06 -0600

    Creacion clase abstracta y clase operaciones

---
+++ EjercicioAbstraccion/Matematicas.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace EjercicioAbstraccion
+{
+    public abstract class Matematicas
+    {
+        double hipotenusa {  get; set; }
+        double catetoOpuesto { get; set; }
+        double catetoContiguo { get; set; }
+
+        public abstract void Imprimir();
+
+        public abstract double Seno();
+        public abstract double Coseno();
+        public abstract double Tangente();
+
+    }
+}
---
+++ EjercicioAbstraccion/Operaciones.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace EjercicioAbstraccion
+{
+    public class Operaciones : Matematicas
+    {
+        public double seno;
+
+        public override double Coseno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Imprimir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double Seno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double Tangente()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
